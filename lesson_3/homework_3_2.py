29e0635340c410459c5ac9f947b24b8ed409bf3b
Ekaterina Kichikova <ekichikova@gmail.com>
2020-08-29 15:25:34 -0400

    homework 3-5

--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -4,20 +4,40 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = 'My dog\'s name is Mishka'
+char_1 = 's'
+result_1 = 0
+i = 0
+
+while i < len(string_1):
+    if (string_1[i]) == char_1:
+        result_1 += 1
+    i = i + 1
+
 
 
 # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
 # and save result in the result_2 variable.
 
-number_1 = None
-result_2 = None
+number_1 = 15622
+string_number_1 = str(number_1)
+result_2 = 1
+ind = 0
 
+while ind < len(string_number_1):
+    result_2 *= int(string_number_1[ind])
+    ind += 1
+print(result_2)
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = 987
+string_number_2 = str(number_2)
+index = len(string_number_2) - 1
+result_3 = ''
+
+while index >= 0:
+    result_3 += string_number_2[index]
+    index = index - 1
+print(result_3)
---
+++ lesson_5/homework_5_1.py
@@ -0,0 +1,44 @@
+# FUNCTIONS
+
+# Difference
+# Write a function, which will calculate the difference of these two numbers
+
+def difference(num_1, num_2):
+    return num_1 - num_2
+
+
+# Division
+# Write a function, which will divide these two numbers
+
+def division(num_1, num_2):
+    return num_1 / num_2
+
+
+# Function gets random number. If this number is more than ten, return the difference between 100 and this number,
+# otherwise return this number multiplied by 10
+
+def function_1(number):
+    if number > 10:
+        return 100 - number
+    else:
+        return number * 10
+
+
+# Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
+# Formula (32°F − 32) × 5/9 = 0°C
+
+def temerature_convertor(fahrenheit_degree):
+    return (fahrenheit_degree - 32) * 5/9
+
+
+# Taxi Fare
+# In a particular jurisdiction, taxi fares consist of a base fare of $4.00, plus $0.25 for every 140 meters travelled.
+# Write a function that takes the distance travelled (in kilometers) as its only parameter and returns the total fare
+# as its only result rounded by 2 digits. Write a program that demonstrates the function.
+
+def taxi_fare(distance):
+    return round((distance / 0.14 * 0.25) + 4.00 , 2)
+
+
+# examples of usage:
+print(taxi_fare(10)) #21.86
