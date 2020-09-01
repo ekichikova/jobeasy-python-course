0f780ea2d14a94ca5e62401905a1534360f22737
Ekaterina Kichikova <ekichikova@gmail.com>
2020-09-01 04:40:35 -0400

    Homework 6

--- lesson_6/homework_6_1.py
+++ lesson_6/homework_6_1.py
@@ -5,7 +5,14 @@
 list_1 = [1, 'asdasd', True, 2, False, 4, 'Hello world', None, range(1, 11), 100]
 
 def swap_first_last(array_1):
-    pass
+    x = array_1[len(array_1) - 1]
+    y = array_1[0]
+    array_1[len(array_1) - 1] = y
+    array_1[0] = x
+    return array_1
+
+
+print(swap_first_last(list_1))
 
 
 # You are given a list in list_2 variable, write a reverse_list function which creates a new list in reversed order.
@@ -13,18 +20,29 @@ def swap_first_last(array_1):
 
 list_2 = [1, 'asdasd', True, 2, False, 4, 'Hello world', None, range(1, 11), 100]
 
+
 def reverse_list(array_2):
-    pass
+    new_arr = array_2[::-1]
+    return new_arr
+
+
+print(reverse_list(list_2))
 
 
 # Create a list which contains only number items and save it to the list_3 variable. Then write multiply_list_items
 # function to multiply all the items in a list. Return result of multiplication
 
-list_3 = None
+list_3 = [2, 10, 5, 2]
 
 def multiply_list_items(array_3):
-    pass
+    item = 1
+    index = 0
+    while index < len(array_3):
+        item *= array_3[index]
+        index += 1
+    return item
 
+print(multiply_list_items(list_3))
 
 # Create a list which contains only number items and save it to the list_4 variable. Then write a smallest_item_list
 # function to get the smallest number from a list. Return smallest element
@@ -32,22 +50,27 @@ def multiply_list_items(array_3):
 list_4 = [10, 2, 5, 5, 0]
 
 def smallest_item_list(array_4):
-    pass
+    return (min(array_4))
 
-
+print(smallest_item_list(list_4))
 # Given a list in list_5 variable, write a remove_duplicates_list function to remove duplicates from a list.
 # Return new list without duplicates
 
 list_5 = [1, 2, 3, 1, 1, 1, 2, 3, 4, 'hello', 1, 2, 3, 4, 'hello', 'hello', 1]
 
 def remove_duplicates_list(array_5):
-    pass
+    array_5 = list(dict.fromkeys(array_5))
+    return array_5
+
+
+print(remove_duplicates_list(list_5))
 
 
 # You are given a list in list_6 variable.Enter an integer number and save it to number_1 variable,
 # write a longer_words_list function which will return the list of words that are longer than number_1
 # from a given list of words.
-number_1 = None
+number_1 = 9
+
 list_6 = ['On', 'it', 'differed', 'repeated', 'wandered', 'required', 'in.', 'Then', 'girl', 'neat', 'why', 'yet',
           'knew', 'rose', 'spot.', 'Moreover', 'property', 'we', 'he', 'kindness', 'greatest', 'be', 'oh', 'striking',
           'laughter.', 'In', 'me', 'he', 'at', 'collecting', 'affronting', 'principles', 'apartments.', 'Has', 'visitor',
@@ -57,8 +80,15 @@ list_6 = ['On', 'it', 'differed', 'repeated', 'wandered', 'required', 'in.', 'Th
           'smallest', 'screened', 'settling.', 'Likely', 'active', 'her', 'warmly', 'has.']
 
 def longer_words_list(array_6, number1):
-    pass
+    new_list = []
+    for item in array_6:
+        if len(item) > number1:
+            new_list.append(item)
 
+    return new_list
+
+
+print(longer_words_list(list_6, number_1))
 
 # Given two lists in list_7 and list_8 variables. Write a function find_item_lists that takes two lists and returns
 # True if they have at least one common member.
@@ -66,29 +96,55 @@ list_7 = [1, 2, 3, 1, 1, 1, 2, 3, 4, 'hello', 1, 2, 3, 4, 'hello', 'hello', 1]
 list_8 = ['asdasd', True, 8, False, 94, 'Hello world', None, range(1, 11), 100, 1]
 
 def find_item_lists(array_7, array_8):
-    pass
+    array_7 = set(array_7)
+    array_8 = set(array_8)
+    if array_7 & array_8:
+        return True
+    else:
+        return False
+
 
 # You are given a list in list_9 variable. Write a function list_to_string to convert a list of
 # characters into a string.
 list_9 = ['I', ' ', 'l', 'i', 'k', 'e', ' ', 'P', 'y', 't', 'h', 'o', 'n']
 
 def list_to_string(list9):
-    pass
+    string = ''
+    index = 0
+    while index < len(list9):
+        string += list9[index]
+        index += 1
+    return string
+
+print(list_to_string(list_9))
+
 
 
 # Given a list of numbers in list_10 and a number number_2, write count_items_list function which will count number of
 # occurrences of number_2 in the given list
 list_10 = [1, 2, 3, 1, 1, 1, 2, 3, 4]
-number_2 = 3
+number_2 = 1
+
 
 def count_items_list(array_10, number2):
-    pass
+    elem_count = array_10.count(number2)
+    return elem_count
+
+print(count_items_list(list_10, number_2))
 
 
 # Given a list of numbers, write a function even_items_list to return new list which include all even numbers in
 # given list.
 list_11 = [1, 2, 3, 1, 1, 1, 2, 3, 4]
 
-def even_items_list(array_11):
-    pass
 
+def even_items_list(array_11):
+    new_array = []
+    index = 0
+    while index < len(array_11):
+        if array_11[index] % 2 == 0:
+            new_array.append(array_11[index])
+        index +=1
+    return new_array
+
+print(even_items_list(list_11))
